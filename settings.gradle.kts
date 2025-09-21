fbb817450bc522815446df4609f182537e0050d5
Luiz Marin <67489841+luizcmarin@users.noreply.github.com>
2025-09-21 17:40:52 -0300

    First update

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+// =============================================================================
+// Arquivo: felynos\settings.gradle.kts
+// Descrição: Configuração do Gradle para o projeto felynos.
+// =============================================================================
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "felynos"
+include(":app")
